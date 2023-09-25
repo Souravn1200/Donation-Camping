97945fec8c8ac37a233fd322d8911a08396a1cca
Souravn1200 <souravn1200@gmail.com>
2023-09-25 22:08:22 +0600

    Made navbar fixed for all the pages

---
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,66 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div>
+            {/* <NavLink to="/"
+            className={({ isActive, isPending }) =>
+            isPending ? "pending" :
+            isActive ? "text-orange-500 underline" : ""}>
+            Home
+            </NavLink>
+
+            <NavLink to="/donation"
+            className={({ isActive, isPending }) =>
+            isPending ? "pending" :
+            isActive ? "text-orange-500 underline" : ""}>
+            Donation
+            </NavLink>
+
+            <NavLink to="/statistics"
+            className={({ isActive, isPending }) =>
+            isPending ? "pending" :
+            isActive ? "text-orange-500 underline" : ""}>
+            Statistics
+            </NavLink> */}
+
+
+
+
+  <div className="flex justify-between items-center mt-5">
+    
+            <div className="w-40">
+            <img src="https://i.ibb.co/SvPW5XH/Logo.png" alt=""/>
+            </div>
+            
+            <div>
+            <ul className="flex gap-4">
+      <li className="mr-4"><NavLink to="/"
+            className={({ isActive, isPending }) =>
+            isPending ? "pending" :
+            isActive ? "text-orange-500 underline" : ""}>
+            Home
+            </NavLink></li>
+      <li className="mr-4"><NavLink to="/donation"
+            className={({ isActive, isPending }) =>
+            isPending ? "pending" :
+            isActive ? "text-orange-500 underline" : ""}>
+            Donation
+            </NavLink></li>
+      <li className="mr-4"><NavLink to="/statistics"
+            className={({ isActive, isPending }) =>
+            isPending ? "pending" :
+            isActive ? "text-orange-500 underline" : ""}>
+            Statistics
+            </NavLink> </li>
+    </ul>
+            </div>
+
+  </div>
+  
+
+        </div>
+    );
+};
+
+export default Navbar;
