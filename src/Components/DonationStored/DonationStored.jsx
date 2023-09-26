1a7bd09786c6e814441ed28adc4614a6963da5da
Souravn1200 <souravn1200@gmail.com>
2023-09-26 11:55:55 +0600

    getting data from local storage and showing it to the donation page with see all button

---
+++ src/Components/DonationStored/DonationStored.jsx
@@ -0,0 +1,28 @@
+
+const DonationStored = ({donation}) => {
+
+    const {picture, title, category, category_bg, card_bg, text_button_bg, description, price } =  donation
+
+    return (
+        <div>
+            <div className="card card-side rounded-md" style={{ backgroundColor: `${card_bg}` }}>
+  <figure><img src={picture} alt="Movie"/></figure>
+  <div className="ml-6 mt-6">
+  <div style={{ backgroundColor: `${category_bg}` }} className='w-auto text-center inline-block px-2 py-1 rounded-md text-xs mt-2'>
+            <p className='' style={{ color: `${text_button_bg}` }}>{category}</p>
+          </div>
+    <h2 className="card-title mt-2">{title}</h2>
+    <p style={{ color: `${text_button_bg}` }}>{price}</p>
+    
+      <button className="py-2 px-2 rounded-md text-white text-sm mt-4" style={{ backgroundColor: `${text_button_bg}` }}>View Details</button>
+
+  </div>
+</div>   
+
+
+
+        </div>
+    );
+};
+
+export default DonationStored;
--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+import DonationStored from "../../Components/DonationStored/DonationStored";
+
 const Donation = () => {
+
+    const [donations, setDonations] = useState([]);
+
+    useEffect(() => {
+        const donationParse = JSON.parse(localStorage.getItem('donationlist'));
+
+        if(donationParse){
+            setDonations(donationParse);
+        }
+
+       
+    }, [])
+
+
     return (
-        <div>
-            this is donation
+        <div> 
+<div className="grid grid-cols-2 gap-10 mt-10">
+            {donations.map( donation => <DonationStored key={donation.id} donation={donation}></DonationStored>)}
         </div>
+        
+<div className="flex justify-center items-center mb-2">
+<button className="py-2 px-2 rounded-md text-white text-sm mt-4 bg-green-700 ">View All</button>
+</div>
+        </div>
+        
     );
 };
 
